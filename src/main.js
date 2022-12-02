026eec18d03a0381cfbf2bb41225e0b199d6780f
Cony GuGa <constanza.garcia.gtz@gmail.com>
2022-12-01 21:10:03 -0600

    Pruebas de Llamado a la data

--- src/main.js
+++ src/main.js
@@ -4,3 +4,13 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+//Quiero mostrar los datos del Pokémon
+
+const tarjetaPokemon = document.getElementById("tarjetaPokemon")
+tarjetaPokemon.innerHTML = `
+<img src="${data.pokemon[0].img}"/>
+<p class="pName">${data.pokemon[0].name}</p> <p id="pNum">${data.pokemon[0].num}</p>
+<p id="pType">${data.pokemon[0].type}</p>
+<p class="pGeneration">${data.pokemon[0].generation.num}${data.pokemon[0].generation.name}`;
+//
