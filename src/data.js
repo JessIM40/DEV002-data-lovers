c35ca2280699c2e20a7c749db592cc2b152b5379
Jess Inga Mayhua <jessinma40@gmail.com>
2022-12-07 09:29:32 -0500

    Modificacion de función showData: uso de .map y . forEach

--- src/data.js
+++ src/data.js
@@ -1,15 +1,21 @@
-// estas funciones son de ejemplo
 
 
 
 
 
 
-export const sortData = () => {
-  
-  return 'example';
-};
 
-export const filterData = () => {
-  return 'OMG';
-};
+// const sortData = (data, sortBy, sortOrder) => {
+//   console.log(data, sortBy, sortOrder);
+//   return console.log('example');
+// };
+
+// const filterData = (data, condition) => {
+//   return 'OMG';
+// };
+
+// const computeStats = (data) => {
+//   return 'OMG';
+// };
+
+// export { sortData };
--- src/main.js
+++ src/main.js
@@ -1,50 +1,77 @@
-import { sortData, filterData } from './data.js';
+// import { sortData } from './data.js';
 import data from './data/pokemon/pokemon.js';
 
-// MOSTRAR TODOS LOS POKEMONES EN TARJETAS
-for(let i=0; i<251; i++){
-    // Traer contenedor de tarjetas Pokemon desde HTML
-    let container = document.getElementById("cardsContainer");
-    // Crear contenedor de datos de cada pokemon
-    let card = document.createElement("div");
-    // Adjuntar/incluir "card" dentro de "container";
-    container.append(card);
-    // Añadir/incluir todos los item de la card Pokemon
-    card.innerHTML = `
-        <img src="${data.pokemon[i].img}"/>
-        <h3 id="pokeName" class="pokeData">${data.pokemon[i].num}//${data.pokemon[i].name}</h3>
-        <h5 id="pokeType" class="pokeData">${data.pokemon[i].type[0]}</h5>
-        <h5 id="pokeGeneration" class="pokeData">${data.pokemon[i].generation.name}//${data.pokemon[i].generation.num}</h5>
-    `
+
+// Traer contenedor de tarjetas Pokemon desde HTML
+let container = document.getElementById("cardsContainer");
+
+// OBTENER PARTE DE LA DATA
+let loadData = data.pokemon.map(function(datum){
+    return {
+        img: datum.img,
+        name: datum.name,
+        num: datum.num,
+        type: datum.type,
+        generation: datum.generation
+    }
+})
+
+// MOSTRAR TODOS LOS POKEMONES EN TARJETAS solo con los datos que trabajaremos
+showData(loadData);
+// funcion que muestra solo data con la que trabajaremos 
+function showData(loadData){
+    loadData.forEach((data) => {
+        // Crear tarjeta (contenedor de datos) de cada pokemon
+        let card = document.createElement("div");
+        // Adjuntar/incluir "card" dentro de "container";
+        container.append(card);
+        // Añadir/incluir todos los item de la card Pokemon
+        card.innerHTML = `
+            <div id="container-pokeImage" class="pokeData">
+                <img src="${data.img}"/>
+            </div>
+            <div id="container-pokeNum_Name" class="pokeData">
+                <h3 id="pokeName">${data.name}</h3>
+                <h3 id="pokeNum">${data.num}</h3>
+            </div>
+            <div id="container-pokeType" class="pokeData">
+                <h5 id="pokeType">${data.type}</h5>
+            </div>
+            <div id="container-pokeGeneration" class="pokeData">
+                <h5 id="pokeGenerationName">${data.generation.name}</h5>
+                <h5 id="pokeGenerationNum">${data.generation.num}</h5>
+            </div>
+        `
+    })
 }
 
+
+// ORDENAR POKEMONS POR TIPO Y NUMERO DE CREACION
 // Traer botones desde HTML y agregar escuchador de eventos
-let btnType = document.getElementById("btnType");
-btnType.addEventListener("click", sortData);
-let btnNum = document.getElementById("btnNum");
-btnNum.addEventListener("click", sortData);
+// let btnType = document.getElementById("btnType");
+// btnType.addEventListener("click", (e)=>{sortData(e, loadData)});
+// let btnNum = document.getElementById("btnNum");
+// btnNum.addEventListener("click", ()=>{sortData(loadData)});
 
 
 
+// function sortData (e, loadData) {
+//     // Definir orden ascendente o descendente
+//     let firstClick = true,
+//         order = firstClick ? ("Orden Ascendente", true) : ("Orden Descendente", false);
 
-/* <h3 id="pokeNum" class="pokeData">${data.pokemon[i].num}</h3> */
+    
+//     if(e.target.id == "btnType" && order == true){
+//         console.log("listo")
 
-// let img = document.createElement("img");
-// let name = document.createElement("h3");
-// let num = document.createElement("h3");
-// let type = document.createElement("h4");
-// let gen = document. createElement("h4");
+//         // loadData.sort(a, b)=>{
+            
+//         // }
+//     }
+//     // loadData.sort((a, b)=>{
+//         // if(a.type < b.type){
+//             // console.log(a.type)
+//         // }
+//     // })
 
-// Agregar atributo
-// let imgAttribute = document.createAttribute("src");
-// imgAttribute.value = data.pokemon[0].img;
-// img.setAttributeNode(imgAttribute);
-
-// card.append(img);
-// card.append(name);
-
-// name.append(data.pokemon[0].name);
-
-
-// console.log(data.pokemon[0].generation.name);
-console.log(sortData, filterData, data);
+// }
